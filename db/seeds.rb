5fe031b30291e109cfeb1a8576608bd199bfeb0f
WHB901 <abdulwhbhussain@gmail.com>
2023-11-28 00:59:22 +0500

    Setup postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Path: hello-rails-react/db/seeds.rb
+
+# create 5 different greeting messages
+
+greetings = [
+      'Hello! How can I assist you today?',
+      'Greetings! I hope you\'re having a great day.',
+      'Hi there! What can I help you with right now?',
+      'Welcome! Feel free to ask me anything.',
+      'Good day! Let me know how I can be of service.'
+    ]
+
+    greetings.each do |greeting|
+      Message.create(greeting: greeting)
+    end
